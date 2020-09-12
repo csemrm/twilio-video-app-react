2410e9bdeba076af1a14d94c3c5773b43fdfac52
Mostafizur Rahman <csemrm@gmail.com>
2020-09-12 08:59:17 +0600

    dd

---
+++ video/precache-manifest.34bd46ee38b35e53928c6c8faa915140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c72fb3ce0f50ccc459aba5b069e3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a1b5e849bf99566e1b",
+    "url": "/static/js/2.20759f69.chunk.js"
+  },
+  {
+    "revision": "8dec489cc9fea776161524e157527d3e",
+    "url": "/static/js/2.20759f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ecec1810b8e404bd08",
+    "url": "/static/js/main.32fb802c.chunk.js"
+  },
+  {
+    "revision": "8f62c64a0ccc5f387d32",
+    "url": "/static/js/runtime-main.d9f235a9.js"
+  },
+  {
+    "revision": "1b8c51ce350862c46a8e50112049fd52",
+    "url": "/static/media/google-logo.1b8c51ce.svg"
+  },
+  {
+    "revision": "0e04ea745e2c6f1fec53ca74a52de526",
+    "url": "/static/media/twilio-logo.0e04ea74.svg"
+  }
+]);
